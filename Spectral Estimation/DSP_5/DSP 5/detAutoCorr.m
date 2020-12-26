68389ceec43bc478b1795493fd5da7599d703d38
DoanCV <72458331+DoanCV@users.noreply.github.com>
2020-12-26 13:46:00 -0500

    Delete detAutoCorr.m

--- Spectral Estimation/DSP_5/DSP 5/detAutoCorr.m
+++
@@ -1,9 +0,0 @@
-function[phi_y1] = detAutoCorr(N,y)
-    phi_y1 = zeros(1,N);
-    y1 = y(1:N);
-    for i = 1:N
-        phi_y1(i) = sum(y1(1:N-i+1) .* y1(i:N)) / N;
-    end
-    phi_y1 = [flip(phi_y1) phi_y1(2:end)];
-
-end
