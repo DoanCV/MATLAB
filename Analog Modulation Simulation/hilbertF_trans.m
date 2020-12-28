0753617f02ee802f860abf6cb07709f24e41143b
DoanCV <72458331+DoanCV@users.noreply.github.com>
2020-12-28 12:56:16 -0500

    Update hilbertF_trans.m

--- Analog Modulation Simulation/hilbertF_trans.m
+++ Analog Modulation Simulation/hilbertF_trans.m
@@ -1,6 +1,3 @@
 function out = hilbertF_trans(signal, freq) %algorithm defined in class, based on definitions
 H_w = -1j .* sign(freq); %F(1/pi*t)
 out = (fftshift(fft(signal)) .* H_w); %real to remove the imginary part introduced to ifft
-
-
-
